e130a8af33d7a2b1de907a786c78b13bd1d9885f
Kevin Chen <kevchen21@gmail.com>
2024-09-20 14:11:23 -0700

    test lambda invoke

--- src/functions/warm-services/handler.ts
+++ src/functions/warm-services/handler.ts
@@ -1,12 +1,22 @@
 import { container } from "./inversify.config";
 import { IColdStartTracker } from "./cold-start-tracker";
+import { InvokeCommand, LambdaClient } from "@aws-sdk/client-lambda";
+
 
 const coldStartTracker = container.get<IColdStartTracker>(IColdStartTracker);
 
 export const main = async (event) => {
-    console.log({ coldStart: coldStartTracker.coldExecutionEnvironment }, JSON.stringify(event, null, 2));
+    console.log(JSON.stringify({ event, coldStart: coldStartTracker.coldExecutionEnvironment }, null, 2));
     coldStartTracker.setFlag();
-    if (!coldStartTracker.coldExecutionEnvironment) { return; }
+    
+    const client = new LambdaClient({ region: "us-west-1" });
 
+    await client.send(new InvokeCommand({
+        FunctionName: "ocr-service-dev-pr-TestTimeFunction-OVptx72kGdvU",
+        InvocationType: 'RequestResponse',
+        Payload: JSON.stringify(event),
+    }));
+
+    if (!coldStartTracker.coldExecutionEnvironment) { return; }
     // Send ping events to the configured endpoints with auth header
 }
