7ad4d3f6c34a90398419d5e5b8698cc010597d9e
Kevin Chen <kevchen21@gmail.com>
2024-09-20 14:52:50 -0700

    test with mock request

--- src/functions/warm-services/handler.ts
+++ src/functions/warm-services/handler.ts
@@ -4,6 +4,50 @@ import { InvokeCommand, LambdaClient } from "@aws-sdk/client-lambda";
 
 
 const coldStartTracker = container.get<IColdStartTracker>(IColdStartTracker);
+const mockRequest = {
+    event: {
+        body: null,
+        headers: {
+            Accept: '*/*',
+            Host: 'localhost:12948',
+            'Content-Type': "application/json",
+            'User-Agent': 'curl/8.7.1',
+            'X-Forwarded-Port': '12948',
+            'X-Forwarded-Proto': 'http'
+        },
+        httpMethod: 'GET',
+        isBase64Encoded: false,
+        multiValueHeaders: {
+            Accept: [Array],
+            Host: [Array],
+            'User-Agent': [Array],
+            'X-Forwarded-Port': [Array],
+            'X-Forwarded-Proto': [Array]
+        },
+        multiValueQueryStringParameters: null,
+        path: '/test',
+        pathParameters: null,
+        queryStringParameters: null,
+        requestContext: {
+            accountId: '123456789012',
+            apiId: '1234567890',
+            domainName: 'localhost:12948',
+            extendedRequestId: null,
+            httpMethod: 'GET',
+            identity: [Object],
+            path: '/test',
+            protocol: 'HTTP/1.1',
+            requestId: '604a2c3c-8170-4290-865c-c86d2826ad6c',
+            requestTime: '20/Sep/2024:21:49:46 +0000',
+            requestTimeEpoch: 1726868986,
+            resourceId: '123456',
+            resourcePath: '/test',
+            stage: 'Stage'
+        },
+        resource: '/test',
+        stageVariables: null
+    }
+};
 
 export const main = async (event) => {
     console.log({ coldStart: coldStartTracker.coldExecutionEnvironment });
@@ -15,7 +59,7 @@ export const main = async (event) => {
     const result = await client.send(new InvokeCommand({
         FunctionName: "ocr-service-dev-pr-TestTimeFunction-OVptx72kGdvU",
         InvocationType: 'RequestResponse',
-        Payload: JSON.stringify(event),
+        Payload: JSON.stringify(mockRequest),
     }));
 
     console.log({
@@ -29,7 +73,7 @@ export const main = async (event) => {
     const processResult = await east1Client.send(new InvokeCommand({
         FunctionName: "ocr-service-dev-pr-TestTimeFunction-OVptx72kGdvU",
         InvocationType: 'RequestResponse',
-        Payload: JSON.stringify({ ping: true }),
+        Payload: JSON.stringify({ ...mockRequest, body: { ping: true } }),
     }));
 
     console.log({
@@ -40,7 +84,7 @@ export const main = async (event) => {
     const getForUserResult = await east1Client.send(new InvokeCommand({
         FunctionName: "expense-service-dev-pr-getForUser",
         InvocationType: 'RequestResponse',
-        Payload: JSON.stringify({ ping: true }),
+        Payload: JSON.stringify({ ...mockRequest, body: { ping: true } }),
     }));
     
     console.log({
