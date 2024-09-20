67b25769cb6586219b04799a8ec881e0629fc1b4
Kevin Chen <kevchen21@gmail.com>
2024-09-20 14:22:11 -0700

    better logging of results

--- src/functions/warm-services/handler.ts
+++ src/functions/warm-services/handler.ts
@@ -6,16 +6,19 @@ import { InvokeCommand, LambdaClient } from "@aws-sdk/client-lambda";
 const coldStartTracker = container.get<IColdStartTracker>(IColdStartTracker);
 
 export const main = async (event) => {
-    console.log(JSON.stringify({ event, coldStart: coldStartTracker.coldExecutionEnvironment }, null, 2));
+    console.log({ coldStart: coldStartTracker.coldExecutionEnvironment });
+    console.log({ event });
     coldStartTracker.setFlag();
-    
+
     const client = new LambdaClient({ region: "us-west-1" });
 
-    await client.send(new InvokeCommand({
+    const result = await client.send(new InvokeCommand({
         FunctionName: "ocr-service-dev-pr-TestTimeFunction-OVptx72kGdvU",
         InvocationType: 'RequestResponse',
         Payload: JSON.stringify(event),
     }));
+
+    console.log({ result });
 
     if (!coldStartTracker.coldExecutionEnvironment) { return; }
     // Send ping events to the configured endpoints with auth header
