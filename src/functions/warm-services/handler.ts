5f493c5917c69c22324af1b0bd26a3422548e3ac
Kevin Chen <kevchen21@gmail.com>
2024-09-22 13:14:52 -0700

    test batch operations

--- src/functions/warm-services/handler.ts
+++ src/functions/warm-services/handler.ts
@@ -1,5 +1,4 @@
 import { ColdStartTracker } from "./utils/cold-start-tracker";
-import { InvokeCommandOutput } from "@aws-sdk/client-lambda";
 import { SNSEvent } from "aws-lambda";
 import { LambdaWarmer } from "./utils/lambda-warmer";
 import { FunctionInfoDao } from "./dao/function-info-dao";
@@ -16,27 +15,14 @@ export const main = async (event: SNSEvent) => {
             return process.env.RtRegion;
         }
     }));
-
-    const invocations: Promise<InvokeCommandOutput>[] = [];
+    
     for (const messageRegion of regions) {
         // if (!coldStartTracker.isColdStart(messageRegion)) { continue; }
         coldStartTracker.setFlag(messageRegion);
 
         // Hit health checks to ensure warm lambda execution environments
         const functionInfos = await dao.listFunctions(messageRegion);
-        invocations.push(...functionInfos.map(async ({ functionName, region }, index) => {
-            try {
-                console.log(`Initiating request ${index} for ${functionName}`);
-                const start = Date.now();
-                const result = await lambdaWarmer.warm(functionName, region);
-                const end = Date.now();
-                console.info({ region, functionName, resultCode: result?.$metadata.httpStatusCode, latency: `${end - start}ms` });
-                return result;
-            } catch (e) {
-                console.error(`Error occurred during request ${index} for ${functionName}`, e);
-            }
-        }));
+        await lambdaWarmer.warmInBatches(functionInfos);
+        
     }
-
-    await Promise.all(invocations);
 }
--- src/functions/warm-services/utils/lambda-warmer.ts
+++ src/functions/warm-services/utils/lambda-warmer.ts
@@ -1,16 +1,41 @@
 import { InvokeCommand, InvokeCommandOutput, LambdaClient } from "@aws-sdk/client-lambda";
 import regionConfiguration from "../configuration/regions.config.json";
+import { FunctionInfo } from "../models/function-info";
 
 export class LambdaWarmer {
+    private readonly BATCH_SIZE = 10;
     private readonly _regions = new Map<string, LambdaClient>();
 
     constructor() {
         for (const region of regionConfiguration.regions) {
             this._regions.set(region, new LambdaClient({ region }));
         }
-    }
+    }  
 
-    warm(functionName: string, region: string): Promise<InvokeCommandOutput> {
+    async warmInBatches(functionInfos: FunctionInfo[]): Promise<void> {
+        const invocations: Promise<InvokeCommandOutput>[] = [];
+
+        for (let index = 0; index < functionInfos.length; index += this.BATCH_SIZE) {
+            const batch = functionInfos.slice(index, index + this.BATCH_SIZE);
+
+            invocations.push(...batch.map(async ({ functionName, region }, index) => {
+                try {
+                    console.log(`Initiating request ${index} for ${functionName}`);
+                    const start = Date.now();
+                    const result = await this.warm(functionName, region);
+                    const end = Date.now();
+                    console.info({ region, functionName, resultCode: result?.$metadata.httpStatusCode, latency: `${end - start}ms` });
+                    return result;
+                } catch (e) {
+                    console.error(`Error occurred during request ${index} for ${functionName}`, e);
+                }
+            }));
+            
+            await Promise.all(invocations);
+        }
+    } 
+
+    private warm(functionName: string, region: string): Promise<InvokeCommandOutput> {
         if (!this._regions.has(region)) {
             console.error(`Region ${region} was not defined in configuration. Function=${functionName}`);
             return Promise.resolve(undefined);
@@ -27,5 +52,5 @@ export class LambdaWarmer {
         });
     
         return this._regions.get(region).send(command);
-    };
+    };   
 }
