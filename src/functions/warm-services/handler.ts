cf61d90d1eb0a313637b4d833f0e34d5037e7de5
Kevin Chen <kevchen21@gmail.com>
2024-09-24 18:57:21 -0700

    favor message region

--- src/functions/warm-services/handler.ts
+++ src/functions/warm-services/handler.ts
@@ -2,16 +2,21 @@ import { ColdStartTracker } from "./utils/cold-start-tracker";
 import { SNSEvent } from "aws-lambda";
 import { LambdaWarmer } from "./utils/lambda-warmer";
 import { FunctionInfoDao } from "./dao/function-info-dao";
+import regionsConfig from "./configuration/regions.config.json";
 
 const coldStartTracker = new ColdStartTracker();
 const lambdaWarmer = new LambdaWarmer();
 const dao = new FunctionInfoDao();
+const availableRegions = new Set(regionsConfig.regions);
 
 export const main = async (event: SNSEvent) => {
     const regions = new Set<string>(event.Records.map(r => {
         try {
             console.log({ message: r.Sns.Message });
-            return JSON.parse(r.Sns.Message).data || process.env.RtRegion
+            const messageRegion = JSON.parse(r.Sns.Message).data;
+            return messageRegion && availableRegions.has(messageRegion)
+                ? messageRegion
+                : process.env.RtRegion;
         } catch {
             return process.env.RtRegion;
         }
